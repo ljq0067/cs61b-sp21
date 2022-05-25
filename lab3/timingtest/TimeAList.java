3c3bbefbb33604bd548d5acc0f483a77264c8e6d
ljq0067 <jieqian_liu@163.com>
2022-05-25 16:30:00 -0400

    Timing the construction of an AList with a bad resize strategy

--- lab3/timingtest/TimeAList.java
+++ lab3/timingtest/TimeAList.java
@@ -3,6 +3,10 @@ import edu.princeton.cs.algs4.Stopwatch;
 
 /**
  * Created by hug.
+ * The first column N gives the size of the data structure (how many elements it contains).
+ * The second column time (s) gives the time required to complete all operations.
+ * The third column # ops gives the number of calls to addLast made during the timing experiment.
+ * The fourth column microsec/op gives the number of microseconds it took on average to complete each call to addLast.
  */
 public class TimeAList {
     private static void printTimingTable(AList<Integer> Ns, AList<Double> times, AList<Integer> opCounts) {
@@ -23,5 +27,24 @@ public class TimeAList {
 
     public static void timeAListConstruction() {
         // TODO: YOUR CODE HERE
+        AList<Integer> N = new AList<>();
+        AList<Integer> Ns = new AList<>();
+        AList<Double> times = new AList<>();
+        AList<Integer> opCounts = new AList<>();
+
+        Stopwatch sw = new Stopwatch();
+        int tick = 0;
+        int ops = 0;
+        for (int i = 0; i < 128000; i += 1) {
+            N.addLast(i);
+            ops += 1;
+            if (N.size() == Math.pow(2, tick) * 1000) {
+                Ns.addLast(N.size());
+                times.addLast(sw.elapsedTime());
+                opCounts.addLast(ops);
+                tick += 1;
+            }
+        }
+        printTimingTable(Ns, times, opCounts);
     }
 }
